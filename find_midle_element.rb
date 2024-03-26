a32037525127c149c27880a9331b3b65a2e7ba8e
Patrick <dongyang0216@gmail.com>
2024-03-26 16:45:12 +0800

    change my answer to make it work even its 5 element or 7 or more element to get the middle array el

--- find_midle_element.rb
+++ find_midle_element.rb
@@ -1,20 +1,29 @@
 #You need to create a function that when provided with a triplet, returns the index of the numerical element that lies between the other two elements.
 #The input to the function will be an array of three distinct numbers(Haskell: a tuple)
 
-
 def gimee(arr)
   first_arr = arr[0]
 
-  if first_arr < arr[1]
-    first_arr > arr[2] ? 0 : first_arr < arr[2] ? 1 : 2
-  else
-    first_arr < arr[2] ? 0 : first_arr > arr[2] ? 1 : 2
+  return 'no middle element' if arr.uniq.length % 2 == 0
+
+  sorted_arr = arr.sort.uniq
+  middle_sorted_arr = sorted_arr[sorted_arr.length / 2.floor]
+
+  arr.uniq.each_with_index do |num,i|
+    return i if num === middle_sorted_arr
   end
+
+  # 1st try when u only find the middle of 3 element
+  # if first_arr < arr[1]
+  #   first_arr > arr[2] ? 0 : first_arr < arr[2] ? 1 : 2
+  # else
+  #   first_arr < arr[2] ? 0 : first_arr > arr[2] ? 1 : 2
+  # end
 end
 
 # Example
-p gimee([2,3,1])    # Output: 0
-p gimee([5,10,14])  # Output: 1
+ p gimee([2,3,1,4,9,7,5,8,4,5,6,6])    # Output: 6
+p gimee([10,5,14])  # Output: 0
 
 
 # Implement reverse method that takes a string as its input and returns its reverse as the output. Avoid using built-in methods that directly solve the problem.
